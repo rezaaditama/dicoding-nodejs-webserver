daba5562a4ba34eb44b9ed2acaf67f56706f46db
Reza Aditama <aditamareza03@gmail.com>
2025-09-30 11:40:19 +0700

    create server

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = 5000;
+const host = 'localhost';
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+  response.statusCode = 200;
+  response.end('<h1>Halo HTTP Server!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () =>
+  console.log(`server berjalan pada http://${host}:${port}`)
+);
