b7ed1aad6a94f8820f012d78ea5ad34afe8436c9
Reza Aditama <aditamareza03@gmail.com>
2025-10-02 10:12:25 +0700

    Routing Request

--- server.js
+++ server.js
@@ -4,19 +4,35 @@ const port = 5000;
 const host = 'localhost';
 
 const requestListener = (request, response) => {
-  response.setHeader('Content-Type', 'aplication/json');
+  response.setHeader('Content-Type', 'text/html');
   response.statusCode = 200;
+  const { method, url } = request;
+  if (url === '/') {
+    if (method === 'GET') {
+      return response.end('Ini adalah homepage');
+    } else {
+      return response.end(
+        `Halaman tidak dapat diakses dengan ${method} request`
+      );
+    }
+  } else if (url === '/about') {
+    if (method === 'GET') {
+      return response.end('Halo! Ini adalah halaman about');
+    }
+    if (method === 'POST') {
+      let body = [];
 
-  const { method } = request;
-  if (method === 'POST') {
-    let body = [];
-
-    request.on('data', (data) => body.push(data));
-    request.on('end', () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}<h1>`);
-    });
+      request.on('data', (data) => body.push(data));
+      request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`Halo, ${name}! Ini adalah halaman about`);
+      });
+    } else {
+      response.end(`Halaman tidak dapat diakses dengan ${method} request`);
+    }
+  } else {
+    response.end('Halaman tidak dapat ditemukan');
   }
 };
 
